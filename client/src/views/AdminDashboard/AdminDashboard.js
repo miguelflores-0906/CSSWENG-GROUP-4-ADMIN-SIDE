45708fad2fa627b13b4ab9dc85175779c5c0b60e
miguelflores-0906 <miggy_4th@yahoo.com>
2021-09-09 18:42:18 +0800

    started on the frontend

---
+++ client/src/views/AdminDashboard/AdminDashboard.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import NavBar from '../NavBar/Navbar'
+import './components/AdminDashboard.css'
+import OngoingOrders from './components/OngoingOrders'
+
+const AdminDashboard = () => {
+    return (
+        <div>
+            <NavBar/>
+            <div className="admin-wrapper">
+                <h1 className= "title ">ADMIN DASHBOARD</h1>
+                <div className="grid-container">
+                    <div className="item1">
+                        Order Tracker <br/>
+                        Order Tracker <br/>
+                        Order Tracker <br/>
+                    </div>
+                    <div className="item2">
+                        Monthly Income <br/>
+                        Monthly Income <br/>
+                        Monthly Income <br/>
+                        Monthly Income <br/>
+                    </div>
+                    <div className="item3">
+                        Breakdown <br/>
+                        Breakdown <br/>
+                    </div>
+                    <div className="item4">
+                        <OngoingOrders/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AdminDashboard
