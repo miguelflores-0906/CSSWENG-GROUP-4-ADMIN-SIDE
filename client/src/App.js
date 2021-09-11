15ba9f42116ada7887519513d1a7ee56c8622121
miguelflores-0906 <miggy_4th@yahoo.com>
2021-09-11 23:47:08 +0800

    finacetrackerview

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import AdminDashboard from './views/AdminDashboard/AdminDashboard'
 import OrderTrackerView from './views/OrderTrackerView/OrderTrackerView'
+import FinanceTrackerView from './views/FinanceTrackerView/FinanceTrackerView'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={AdminDashboard}/>
           <Route path="/order" exact component={OrderTrackerView}/>
+          <Route path="/finance" exact component={FinanceTrackerView}/>
         </Switch>
       </div>
     </Router>
---
+++ client/src/views/FinanceTrackerView/FinanceTrackerView.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Components/FinanceTrackerView.css'
+import NavBar from '../NavBar/Navbar'
+
+const FinanceTrackerView = () => {
+    return (
+        <div>
+            <NavBar/>
+            <div className="main-wrapper">
+
+            </div>
+        </div>
+    )
+}
+
+export default FinanceTrackerView
