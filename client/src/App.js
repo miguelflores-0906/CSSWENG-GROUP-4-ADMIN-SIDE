0579ea0f07d3a5c18d6f8c233df3e4c8b88458fb
miguelflores-0906 <miggy_4th@yahoo.com>
2021-09-12 10:44:16 +0800

    added forms frontend

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import AdminDashboard from './views/AdminDashboard/AdminDashboard'
 import OrderTrackerView from './views/OrderTrackerView/OrderTrackerView'
 import FinanceTrackerView from './views/FinanceTrackerView/FinanceTrackerView'
+import AddExpense from './views/Forms/AddExpense'
+import AddRevenue from './views/Forms/AddRevenue'
+import AddItem from './views/Forms/AddItem';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
           <Route path="/" exact component={AdminDashboard}/>
           <Route path="/order" exact component={OrderTrackerView}/>
           <Route path="/finance" exact component={FinanceTrackerView}/>
+          <Route path="/addExpense" exact component={AddExpense}/>
+          <Route path="/addRevenue" exact component={AddRevenue}/>
+          <Route path="/addItem" exact component={AddItem}/>
         </Switch>
       </div>
     </Router>
---
+++ client/src/views/Forms/AddItem.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import './forms.css'
+
+const AddItem = () => {
+    return (
+        <div>
+            <div className="form-card">
+                <h1>ADD ITEM</h1>
+                <form id="addItem" className="form-wrapper">
+                    <label>MATERIAL:</label>
+                    <select id="add-material" name="add-material">
+                        <option value="glass">GLASS</option>
+                        <option value="aluminum">ALUMINUM</option>
+                    </select>
+                    <br/>
+                    <label>HEIGHT (in):</label>
+                    <input type="text" name="add-item-height"/>
+                    <br/>
+                    <label>WIDTH (in):</label>
+                    <input type="text" name="add-item-width"/>
+                    <br/>
+                    <label>LENGTH (in):</label>
+                    <input type="text" name="add-item-length"/>
+                    <br/>
+                    <label>THICKNESS (in):</label>
+                    <input type="text" name="add-item-thickness"/>
+                    <br/>
+                    <label>COLOR:</label>
+                    <input type="text" name="add-item-color"/>
+                    <br/>
+                    <label>QUANTITY:</label>
+                    <input type="text" name="add-item-qty"/>
+                    <br/>
+                </form>
+                <div className="form-buttons">
+                    <button className="delete-btn">CANCEL</button>
+                    <button className="save-btn" type="submit" form="addRevenue">ADD</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AddItem
